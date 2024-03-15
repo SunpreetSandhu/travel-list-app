5dc2fefbc746bf6c2b93a3b189e063233d49fd37
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-03-15 13:09:13 -0400

    Added controlled elements for various inputs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 12, packed: false },
 ];
 
 function App() {
@@ -19,10 +20,36 @@ function Logo() {
   return <h1>🏝️ Travel List</h1>;
 }
 function Form() {
+  const [desc, setDesc] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!desc) return;
+    const newItem = { desc, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDesc("");
+    setQuantity(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+      />
+      <button>ADD</button>
+    </form>
   );
 }
 
@@ -31,7 +58,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -42,8 +69,7 @@ function Item({ item }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity}
-        {item.description}
+        {item.quantity} {item.description}
       </span>
       <button>❌</button>
     </li>
